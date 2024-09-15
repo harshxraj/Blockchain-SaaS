cfc262ff40bf48f9b9c29870f0d3f066f2a00206
harshxraj <harshraj0588@gmail.com>
2024-09-16 01:48:58 +0530

    features grid section done

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,5 +1,6 @@
 import { twMerge } from "tailwind-merge";
 import TextButton from "../components/TextButton";
+import { Card } from "../components/Card";
 
 const cardData = [
   {
@@ -42,24 +43,7 @@ export const FeaturesSection = () => {
         <div className="mt-36 flex lg:mt-48">
           <div className="flex flex-none gap-8">
             {cardData.map(({ image, title, description, color }) => (
-              <div key={title} className="relative z-0 p-8 md:p-10 max-w-xs md:max-w-md group">
-                <div
-                  className={twMerge(
-                    "absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300",
-                    color == "lime" && "bg-lime-500",
-                    color == "violet" && "bg-violet-500",
-                    color == "cyan" && "bg-cyan-500"
-                  )}
-                ></div>
-                <div
-                  className={twMerge(
-                    "absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 group-hover:bg-fuchsia-400 transition duration-300",
-                    color == "lime" && "bg-lime-500 group-hover:bg-lime-400",
-                    color == "violet" && "bg-violet-500 group-hover:bg-violet-400",
-                    color == "cyan" && "bg-cyan-500 group-hover:bg-cyan-400"
-                  )}
-                ></div>
-                <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
+              <Card key={title} color={color} className="max-w-xs md:max-w-md">
                 <div className="flex justify-center -mt-28">
                   <div className="inline-flex relative ">
                     <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
@@ -72,24 +56,7 @@ export const FeaturesSection = () => {
                 </div>
                 <h3 className="font-heading font-black text-3xl mt-12">{title}</h3>
                 <p className="text-lg text-zinc-400 mt-4">{description}</p>
-                <div className="flex justify-between mt-12">
-                  <TextButton color={color}>Learn More</TextButton>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="currentColor"
-                    className="size-8 text-zinc-500 group-hover:text-zinc-300 transition duration-300 -translate-x-2 group-hover:translate-x-0"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
-                    />
-                  </svg>
-                </div>
-              </div>
+              </Card>
             ))}
           </div>
         </div>
---
+++ src/sections/LatestPosts.tsx
@@ -0,0 +1,36 @@
+import type { CollectionEntry } from "astro:content";
+import { Card } from "../components/Card";
+
+export const LatestPosts = (props: { latestPosts: CollectionEntry<"blog">[] }) => {
+  return (
+    <section className="py-60">
+      <div className="container">
+        <h2 className="font-heading font-black text-4xl text-center">
+          Your portal to everything blockchain.
+        </h2>
+        <p className="text-xl text-center text-zinc-400 mt-8">
+          Keep up with the newest trends, updates, and insights in the blockchain world, updated weekly.
+        </p>
+
+        <div className="mt-16 flex flex-col gap-8">
+          {[
+            ...new Array(4).fill(0).map((item, itemIndex) => (
+              <Card key={itemIndex} buttonText="Read More" className="">
+                <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
+                  Technology
+                </div>
+                <h3 className="font-heading font-black text-3xl mt-4">
+                  Regulatory Challenges Facing Blockchain
+                </h3>
+                <p className="text-lg text-zinc-400 mt-6">
+                  Understanding the regulatory landscape surrounding blockchain and what it means for the
+                  future of this technology.
+                </p>
+              </Card>
+            )),
+          ]}
+        </div>
+      </div>
+    </section>
+  );
+};
